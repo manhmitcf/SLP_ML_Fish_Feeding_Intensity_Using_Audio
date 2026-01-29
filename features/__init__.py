c364ab0aecd1b12904595041d8710016cfd04c4d
manhmitcf <manhmit1221@gmail.com>
2026-01-29 17:32:55 +0700

    add: add 'STFTConfig','FFTConfig','STFTExtractor''FFTExtractor' in init

--- features/__init__.py
+++ features/__init__.py
@@ -1,12 +1,18 @@
-from .config import BaseFeatureConfig, MFCCConfig
+from .config import BaseFeatureConfig, MFCCConfig, STFTConfig, FFTConfig
 from .base import BaseFeatureExtractor
 from .mfcc import MFCCExtractor
+from .stft import STFTExtractor
+from .fft import FFTExtractor
 from .manager import FeatureManager
 
 __all__ = [
     'BaseFeatureConfig', 
     'MFCCConfig', 
+    'STFTConfig',
+    'FFTConfig',
     'BaseFeatureExtractor', 
     'MFCCExtractor', 
+    'STFTExtractor',
+    'FFTExtractor',
     'FeatureManager'
 ]
